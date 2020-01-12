2c6a6f6e9b4e7c2d0a12a7c630a178823e276f24
barshaid <exofang@hotmail.com>
2020-01-12 16:19:19 +0200

    rebuild ver, game editor focus

---
+++ Assets/LevelGenerator.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+
+	public Texture2D map;
+
+	public ColorToPrefab[] colorMappings;
+
+	// Use this for initialization
+	void Start()
+	{
+		GenerateLevel();
+	}
+
+	void GenerateLevel()
+	{
+		for (int x = 0; x < map.width; x++)
+		{
+			for (int y = 0; y < map.height; y++)
+			{
+				GenerateTile(x, y);
+			}
+		}
+	}
+
+	void GenerateTile(int x, int y)
+	{
+		Color pixelColor = map.GetPixel(x, y);
+
+		if (pixelColor.a == 0)
+		{
+			Debug.Log("transparent");
+			// The pixel is transparrent. Let's ignore it!
+			return;
+		}
+
+		foreach (ColorToPrefab colorMapping in colorMappings)
+		{
+			Vector2 position = new Vector2(x*5, y);
+			Instantiate(colorMapping.prefab, position, Quaternion.identity, transform);
+			if (colorMapping.color.Equals(pixelColor))
+			{
+				
+				//Vector2 position = new Vector2(x, y);
+				//Instantiate(colorMapping.prefab, position, Quaternion.identity, transform);
+			}
+		}
+	}
+
+}
+
---
+++ Assets/Movement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    Rigidbody2D rb;
+    public Transform _groundCheck;
+    bool _canJump, _canWalk;
+    bool _isWalk, _isJump;
+    RaycastHit2D _hit;
+
+
+    float dir;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, _groundCheck.position);
+    }
+
+    void Update()
+    {
+        dir = CrossPlatformInputManager.GetAxis("Horizontal") * moveSpeed;
+        rb.velocity = new Vector2(dir, rb.velocity.y);
+
+        if (CrossPlatformInputManager.GetButtonDown("Jump") && rb.velocity.y<0.001 && rb.velocity.y>-0.001)
+        {
+            rb.AddForce(Vector2.up * 500f);
+        }
+
+        if (_hit = Physics2D.Linecast(new Vector2(_groundCheck.position.x, _groundCheck.position.y + 0.2f), _groundCheck.position))
+        {
+            if (!_hit.transform.CompareTag("Player"))
+            {
+                _canJump = true;
+                _canWalk = true;
+            }
+        }
+        else _canJump = false;
+    }
+
+}
