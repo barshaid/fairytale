f66367c29c5762e3072076f3bbed614b0ebc7858
barshaid <exofang@hotmail.com>
2020-02-26 14:55:02 +0200

    fixed paralax, fixed clipping between platform colliders

--- Assets/CaveLight.cs
+++ Assets/CaveLight.cs
@@ -9,24 +9,26 @@ public class CaveLight : MonoBehaviour
 
     private void Start()
     {
-        dirLight = FindObjectsOfType<Light>()[1];
+        dirLight = FindObjectsOfType<Light>()[0];
     }
-
 
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.CompareTag("Player"))
         {
-            col.gameObject.GetComponentInChildren<Light>().intensity = 10f;
             dirLight.intensity = 0;
+            col.gameObject.GetComponentInChildren<Light>().intensity = 10f;
+
         }
     }
+
     private void OnTriggerStay2D(Collider2D col)
     {
         if (col.CompareTag("Player"))
         {
-            col.gameObject.GetComponentInChildren<Light>().intensity = 10f;
             dirLight.intensity = 0;
+            col.gameObject.GetComponentInChildren<Light>().intensity = 10f;
+            
         }
     }
 
